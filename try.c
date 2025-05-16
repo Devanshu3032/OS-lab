e4570e5950515b5bcaf33f72f920f8b6a798ad1c
Devanshu3032 <devanshukumar841@gmail.com>
2025-05-16 23:56:50 +0530

    "da"

--- try.c
+++ try.c
@@ -1,16 +1,7 @@
-#include<stdio.h>
-#include<dirent.h>
+#include<stdio.h> 
+#include<dirent.h> 
 struct dirent *a ;
 int main(){
-    DIR *b ;
-    b = opendir("./home");
-    if(b = NULL){
-        perror("ERROR PROCESS EXECUTION");
-        return 1 ;
-    }
-    while((a = readdir(b)) != 0 ){
-        printf("%s\n",a ->d_name);
-    }
-    closedir(b);
-    return 1 ;
+    DIR *b ; 
+    b = opendir
 }
