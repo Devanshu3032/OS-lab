dc725af33bcae3cc283174fc2d3152423fae18cf
Devanshu3032 <devanshukumar841@gmail.com>
2025-05-17 23:38:03 +0530

    "l;"

--- try.c
+++ try.c
@@ -8,7 +8,7 @@ int main(){
         perror("Errorr");
     }
     while(( a= readdir(b)) != 0){
-        printf("%s\n", a -> d_name);
+        printf("%s", a -> d_name);
     }
     closedir(b) ;
 }
